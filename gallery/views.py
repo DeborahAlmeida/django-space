a9668349b4a5230341350eb538191a780dfeea7a
Deborah Mendonca <deborahsoares01@gmail.com>
2024-02-24 23:08:18 -0300

    adding templates and static files

---
+++ gallery/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'gallery/index.html')
+
+def image(request):
+    return render(request, 'gallery/image.html')
