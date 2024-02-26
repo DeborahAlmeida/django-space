fb8052a05afb649122d425ef6a0ea69836853ba4
Deborah Mendonca <deborahsoares01@gmail.com>
2024-02-26 00:26:05 -0300

    updating

--- gallery/views.py
+++ gallery/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from gallery.models import Fotografia
 
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
 
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     return render(request, 'gallery/index.html', {'cards': fotografias })
 
 def image(request, foto_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'gallery/image.html', {"fotografia": fotografia})
 
 def buscar(request):
-
+    if not request.user.is_authenticated:
+        return redirect('login')
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     if "value" in request.GET:
